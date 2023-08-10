b38871906a3d68a523a7ec4e8f4debed854c5af2
adrewtran117 <adrewtran117@gmail.com>
2023-08-10 18:31:36 -0400

    add team

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
-var tabPanels=document.querySelectorAll(".tabContainer .tabPanel");
+var tabButtons=document.querySelectorAll(".teamTabContainer .teamButtonContainer button");
+var tabPanels=document.querySelectorAll(".teamTabContainer .teamTabPanel");
 
-function showPanel(panelIndex, colorCode) {
+function showPanel(panelIndex) {
     tabButtons.forEach(function(node) {
         node.style.backgroundColor="";
         node.style.color="";
     });
-    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    // tabButtons[panelIndex].style.backgroundColor=colorCode;
     tabButtons[panelIndex].style.color="white";
     tabPanels.forEach(function(node) {
         node.style.display="none";
     });
     tabPanels[panelIndex].style.display="block";
-    tabPanels[panelIndex].style.backgroundColor=colorCode;
+    // tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
+
+showPanel(0);
