d985da0f6aff49c55d15a7ee8b19d792c6c5fac7
devinrankin <tun80253@temple.edu>
2024-05-13 15:01:13 -0400

    changed splashscreen to dynamic to fix ssr issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,25 @@ import Team from "./_components/Team/Team";
 import Sponsors from "./_components/Sponsors/Sponsors";
 import SplashScreen from "./_components/SplashScreen/SplashScreen";
 import Footer from "./_components/Footer/Footer";
+import dynamic from "next/dynamic";
 
 type Props = {};
+
+const DynamicSplashScreen = dynamic(
+  () => import("./_components/SplashScreen/SplashScreen"),
+  { ssr: false }
+);
 
 export default function page({}: Props) {
   return (
     <main className="">
-        <SplashScreen />
-        <Hero />
-        <About />
-        <Sponsors />
-        <FAQ />
-        <Team />
-        <Footer />
+      <DynamicSplashScreen />
+      <Hero />
+      <About />
+      <Sponsors />
+      <FAQ />
+      <Team />
+      <Footer />
     </main>
   );
 }
