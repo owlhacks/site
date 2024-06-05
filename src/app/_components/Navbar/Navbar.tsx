8bb255c92930df35b767d2120b8c803aae899ef2
devinrankin <tun80253@temple.edu>
2024-06-04 20:55:53 -0400

    fixed mobile navbar appearing during load and made hamburger fixed to the page

--- src/app/_components/Navbar/Navbar.tsx
+++ src/app/_components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { scrollToSection } from "@/app/_utilities/scrolling";
 import Text from "../Typography/Text";
 
 import { MouseEventHandler, ReactNode, useState } from "react";
-import { motion, transform } from "framer-motion";
+import { AnimatePresence, easeIn, motion, transform } from "framer-motion";
 
 type NavLinkProps = {
   heading: string;
@@ -73,14 +73,16 @@ const bottomLine = {
 
 const drawer = {
   open: {
-    x: 0,
+    x: ["-100%", 0],
     transition: {
+      duration: 0.25,
       type: "tween",
     },
   },
   closed: {
     x: "-100%",
     transition: {
+      duration: 0.25,
       type: "tween",
     },
   },
@@ -129,9 +131,9 @@ export default function Navigation() {
   return (
     <>
       {/* mobile navbar */}
-      <nav className="sm:hidden block absolute z-40">
+      <nav className="sm:hidden block z-40 fixed">
         <motion.div
-          className={`space-y-1 rounded-br-lg p-7 absolute ${
+          className={`space-y-1 rounded-br-lg p-7 absolute z-50 ${
             !isOpen && "bg-skin-base"
           }`}
           onClick={() => {
@@ -154,30 +156,37 @@ export default function Navigation() {
             animate={isOpen ? "open" : "closed"}
           />
         </motion.div>
-        <motion.div
-          className="h-full w-3/4 bg-skin-base fixed opacity-95"
-          variants={drawer}
-          animate={isOpen ? "open" : "closed"}
-        >
-          <ul className="flex flex-col h-full justify-center items-start space-y-5 mx-10">
-            <NavLink heading="About" href="about" />
-            <NavLink heading="Sponsors" href="sponsors" />
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              className={`h-full bg-skin-base fixed opacity-95 ease-in-out ${
+                isOpen ? "w-3/4" : "w-0 overflow-hidden"
+              }`}
+              variants={drawer}
+              animate="open"
+              exit="closed"
+            >
+              <ul className="flex flex-col h-full justify-center items-start space-y-5 mx-10">
+                <NavLink heading="About" href="about" />
+                <NavLink heading="Sponsors" href="sponsors" />
 
-            <NavLink heading="FAQ" href="FAQ" />
+                <NavLink heading="FAQ" href="FAQ" />
 
-            <NavLink heading="Team" href="team" />
-            <NavLink
-              heading="Sign up with"
-              svgProps={{
-                viewBox: "0 0 640 512",
-                path: "M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z",
-                label: "Discord",
-              }}
-              href="/login"
-              border
-            />
-          </ul>
-        </motion.div>
+                <NavLink heading="Team" href="team" />
+                <NavLink
+                  heading="Sign up with"
+                  svgProps={{
+                    viewBox: "0 0 640 512",
+                    path: "M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z",
+                    label: "Discord",
+                  }}
+                  href="/login"
+                  border
+                />
+              </ul>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </nav>
 
       {/* desktop/tablet navbar */}
