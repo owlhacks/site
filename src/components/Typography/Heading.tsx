ca3f8a4cb961ba4b79b596646255507a22cf5862
devinrankin <tun80253@temple.edu>
2024-07-22 20:49:07 -0400

    updates to heading

--- src/components/Typography/Heading.tsx
+++ src/components/Typography/Heading.tsx
@@ -1,14 +1,18 @@
 import React, { ReactNode } from "react";
 
-type HeadingProps = {
+type HeadingProps = React.HTMLProps<HTMLHeadingElement> & {
   variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   children: ReactNode;
   className?: string;
 };
 
 export default function Heading(props: HeadingProps) {
-  const { variant, children, className } = props;
+  const { variant, children, className, ...headingProps } = props;
   const HeadingVariant = variant;
 
-  return <HeadingVariant className={className}>{children}</HeadingVariant>;
+  return (
+    <HeadingVariant className={className} {...headingProps}>
+      {children}
+    </HeadingVariant>
+  );
 }
