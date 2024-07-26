5fca211fbc4a992d5750419fa1373614d681eb6b
devinrankin <tun80253@temple.edu>
2024-07-25 20:00:11 -0400

    removed mlh badge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,20 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <a id="mlh-trust-badge"
-        style = {{
-          display: "block",
-          maxWidth: "100px",
-          minWidth: "60px",
-          position: "fixed",
-          right: "25px",
-          top: "0",
-          width: "15%",
-          zIndex: 10000
-        }}
-        href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white" target="_blank">
-          <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-white.svg" alt="Major League Hacking 2025 Hackathon Season" style={{width:"100%"}}/>
-      </a>
       <body className={poppins.className}>
         <Providers>{children}</Providers>
       </body>
