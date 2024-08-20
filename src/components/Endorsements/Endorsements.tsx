b3d765fd441a377d05c8703a182302c6e0ec10ce
Phuykong <m.phuykong@gmail.com>
2024-08-19 20:49:41 -0400

    added endorsements

--- src/components/Endorsements/Endorsements.tsx
+++ src/components/Endorsements/Endorsements.tsx
@@ -20,6 +20,33 @@ export default function Endorsements({}: Props) {
         OwlHacks is brought to life by its endorsers.
       </Text>
 
+      <div className="flex flex-wrap gap-4">
+        <Link
+          aria-label="Temple Trading Technologies"
+          href="https://www.instagram.com/templetradingtech/" target="_blank">
+          <Image
+            className="w-52 h-52"
+            src="/endorsement_logo/temple_trading_tech.webp"
+            alt="Temple Trading Technologies"
+            width={300}
+            height={300}
+          />
+        </Link>
+        <Link
+          aria-label="Temple Robotics"
+          href="https://www.instagram.com/templerobotics/" target="_blank">
+          <Image
+            className="w-96 h-52"
+            src="/endorsement_logo/temple_robotics.webp"
+            alt="Temple Robotics"
+            width={300}
+            height={200}
+          />
+        </Link>
+      </div>
+
+
+
     </SectionContent>
   );
 }
